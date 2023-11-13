0c7119b31b1e71d6f3c483828764efcd2a561cfe
Michael Royal <45159366+mikeroyal@users.noreply.github.com>
2023-11-13 14:29:19 -0800

    Updated Getting Started with ISP Development.py

--- Getting Started with ISP Development.py
+++ Getting Started with ISP Development.py
@@ -1,1 +1,9 @@
 Code samples & snippets coming soon!
+
+import numpy as np
+from scipy import signal, datasets
+import matplotlib.pyplot as plt
+
+image = datasets.face(gray=True).astype(np.float32)
+derfilt = np.array([2.0, -1, 2.0], dtype=np.float32)
+ck = signal.cspline2d(image, 6.0)
